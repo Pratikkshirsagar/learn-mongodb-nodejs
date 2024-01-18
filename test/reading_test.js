a248dd70184db585b00062f3836d4fbb116436a2
pratikkshirsagar <meetpratiik1995@gmail.com>
2024-01-18 19:31:34 +0530

    pagination added

--- test/reading_test.js
+++ test/reading_test.js
@@ -3,11 +3,14 @@ const User = require('../src/user');
 const { beforeEach } = require('mocha');
 
 describe('Reding users out of the database', async () => {
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach(async () => {
     try {
+      alex = await User.create({ name: 'Alex' });
       joe = await User.create({ name: 'Joe' });
+      maria = await User.create({ name: 'Maria' });
+      zach = await User.create({ name: 'Zach' });
     } catch (err) {
       console.log(err);
     }
@@ -28,6 +31,18 @@ describe('Reding users out of the database', async () => {
       const user = await User.findOne({ _id: joe._id });
 
       assert(user.name === 'Joe');
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  it('can skip and limit the result set', async () => {
+    try {
+      const users = await User.find({}).sort({ name: 1 }).skip(1).limit(2);
+
+      assert(users.length === 2);
+      assert(users[0].name === 'Joe');
+      assert(users[1].name === 'Maria');
     } catch (err) {
       console.log(err);
     }
